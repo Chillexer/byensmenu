2d1f38aebaffeb0f1ffedd18e512b7bb5bc7f64f
FRFN (Frederik Foss Nielsen) <FRFN@nnit.com>
2023-01-09 17:23:49 +0100

    Made search go down

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useLiveQuery } from "dexie-react-hooks";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
@@ -9,6 +9,13 @@ function App() {
 	const categories = useLiveQuery(() => db.categories.toArray());
 	const [search, setSearch] = useState("");
 	const [initialized, setInitialized] = useState(false);
+	const [searchOffset, setSearchOffset] = useState(0);
+	const [offset, setOffset] = useState(0);
+	const searchInput = useCallback((node: HTMLDivElement) => {
+		if (node !== null) {
+			setSearchOffset(node.offsetTop);
+		}
+	}, []);
 
 	useEffect(() => {
 		async function initialize() {
@@ -17,14 +24,31 @@ function App() {
 		}
 
 		initialize();
+
+		const onScroll = () => {
+			setOffset(window.scrollY);
+		};
+		window.removeEventListener("scroll", onScroll);
+		window.addEventListener("scroll", onScroll, { passive: true });
+
+		return () => window.removeEventListener("scroll", onScroll);
 	}, []);
 
 	if (initialized)
 		return (
 			<div className="bg-grey-200">
 				<Navbar />
-
-				<input type="text" onChange={(e) => setSearch(e.target.value)}></input>
+				<div
+					className={`bg-white w-full flex items-center shadow-lg justify-between h-20 px-4 md:px-24 lg:px-48 xl:px-96 ${
+						offset > searchOffset ? "fixed top-0" : ""
+					}`}
+					ref={searchInput}>
+					<input
+						className="w-full border border-gray-400 focus:border-gray-700 rounded-xl px-4 py-2"
+						type="text"
+						placeholder="Søg"
+						onChange={(e) => setSearch(e.target.value)}></input>
+				</div>
 
 				{categories?.map((category) => (
 					<Section search={search} category={category} key={category.id} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Navbar({}: Props) {
 	return (
-		<header className="flex items-center justify-between h-20 px-4 text-3xl shadow-lg md:px-24 lg:px-48 xl:px-96">
+		<header className="flex items-center justify-between h-20 px-4 text-3xl md:px-24 lg:px-48 xl:px-96">
 			<p className="font-semibold">Menu</p>
 			<img className="h-14" alt="logo" src={logo} />
 		</header>
